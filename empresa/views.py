65f60ae5bb3aa4081f3cb3e629a6652d62c70731
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2025-11-03 11:08:05 -0300

    Criação da tela de vagas da empresa e criação do base da empresa

--- empresa/views.py
+++ empresa/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'empresa/base.html')
+    return render(request, 'empresa/base_empresa.html')
+
+def tela_principal_empresa(request):
+    return render(request, 'empresa/tela_principal_empresa.html')
+
+def perfil_empresa(request):
+    return render(request, 'empresa/perfil.html') 
+
+def cadastrar_vagas(request):
+    return render(request, 'empresa/cadastrar_vagas.html')
+
+def vagas_empresa(request):
+    return render(request, 'empresa/vagas.html')
