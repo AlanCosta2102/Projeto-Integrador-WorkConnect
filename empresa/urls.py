b19347ea251eacfa096611de96c3e669c5261e52
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2025-11-05 14:19:48 -0300

    Criação da tela de vagas da empresa e criação do base da empresa

--- empresa/urls.py
+++ empresa/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('cadastrar_vagas/', views.cadastrar_vagas, name='cadastrar_vagas'),
     
     path('vagas/', views.vagas_empresa, name='vagas_empresa'),
+
+    path('lista_candidatos/', views.lista_candidatos, name='lista_candidatos')
 ]
