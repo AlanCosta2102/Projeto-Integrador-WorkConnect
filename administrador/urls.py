e020d2514950d001cdcdc5ca96046398ed946cc6
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2025-11-07 16:15:39 -0300

    Criação de tela de morerar vagas(Administrador)

--- administrador/urls.py
+++ administrador/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  
     path('gerenciamento/', views.gerenciamento_view, name='gerenciamento'),
+    path('tela_moderar_vagas/',views.tela_moderar_vagas_view,name='tela_moderar_vagas'),
 ]
