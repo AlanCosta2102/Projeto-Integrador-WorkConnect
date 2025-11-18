e5d42fb338cd4c8f1dca864a505a496303210068
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2025-11-18 13:30:19 -0300

    Subindo tela de seleção para Pessoa ou Empresa e responsividade das telas

--- administrador/urls.py
+++ administrador/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'administrador'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  
     path('gerenciamento/', views.gerenciamento_view, name='gerenciamento'),
--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('tela_selecao/',views.tela_selecao,name='tela_selecao'),
 ]
