74ea07f5915da2763d1cbb9fb804697bedc5bede
AlanCosta2102 <costa.alan@escolar.ifrn.edu.br>
2026-01-07 15:10:44 -0300

    Atualizações de últimas vagas para candidato

--- candidato/urls.py
+++ candidato/urls.py
@@ -5,21 +5,19 @@ app_name = 'candidato'
 
 urlpatterns = [
     path('tela_principal_candidato/', views.tela_principal_candidato, name='tela_principal_candidato'), 
-    
-    # path('perfil/', views.perfil_candidato, name='perfil_candidato'),
-    
+        
     path('candidaturas/', views.candidaturas_candidato, name='candidaturas_candidato'),
     
     path('vagas/', views.vagas_candidato, name='vagas_candidato'),
 
     path('criar_conta/', views.criar_conta, name='criar_conta'),   
 
-    path('descricao_vaga/', views.descricao_vaga, name= 'descricao_vaga'),
-
     path('perfil_candidato/', views.perfil_candidato, name='perfil_candidato'),
 
      path('perfil_empresa/', views.perfil_empresa, name='perfil_empresa'),
 
-     path('inscricao/',views.inscricao,name='inscricao'),
+     path('inscricao/<int:vaga_id>/',views.inscricao,name='inscricao'),
+
+     path('vaga/<int:vaga_id>/',views.detalhes_vaga,name='detalhes_vaga'),
 
 ]
--- candidato/views.py
+++ candidato/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from core.models import Usuario
 from candidato.models import Candidato
+from empresa.models import Vaga
 from django.contrib import messages
 import re
 
@@ -10,16 +11,28 @@ def index(request):
 
 @login_required(login_url='login')
 def tela_principal_candidato(request):
-    return render(request, 'candidato/tela_principal_candidato.html')
+    if request.user.tipo_usuario != 'candidato':
+        return redirect('login')
+    
+    ultimas_vagas = (
+        Vaga.objects.filter(ativa=True)
+        .order_by('-criada_em')[:9]
+    )
+    return render(request, 'candidato/tela_principal_candidato.html',{
+        'ultimas_vagas':ultimas_vagas
+    })
 
-# def perfil_candidato(request):
-#     return render(request, 'candidato/perfil.html') 
 
 def candidaturas_candidato(request):
     return render(request, 'candidato/candidaturas.html')
 
+@login_required(login_url='login')
 def vagas_candidato(request):
-    return render(request, 'candidato/vagas.html')
+    vagas =Vaga.objects.filter(ativa=True).order_by('-criada_em')
+
+    return render(request, 'candidato/vagas.html',{
+         'vagas':vagas
+    })
 
 def criar_conta(request):
     if request.method == 'POST':
@@ -62,14 +75,25 @@ def criar_conta(request):
 
     return render(request, 'candidato/criar_conta.html')
 
-def descricao_vaga(request):
-    return render(request, 'candidato/descricao_vaga.html')
-
 def perfil_candidato(request):
     return render(request, 'candidato/perfil_candidato.html')
 
 def perfil_empresa(request):
     return render(request, 'candidato/perfil_empresa.html')
 
-def inscricao(request):
+@login_required
+def inscricao(request, vaga_id):
+    vaga = get_object_or_404(Vaga, id=vaga_id, ativa=True)
+
+    if request.method == 'POST':
+
+     return redirect('candidato:inscricao', vaga_id=vaga.id)
+    
     return render(request,'candidato/inscricao.html')
+
+def detalhes_vaga(request,vaga_id):
+    vaga = get_object_or_404(Vaga,id=vaga_id,ativa=True)
+
+    return render(request,'candidato/detalhes_vaga.html',{
+        'vaga':vaga
+    })
